8c09b235a9791a53583b04d2dc0cdf660a64ff40
Steveh77 <stederosa76@gmail.com>
2022-06-23 17:02:09 +0200

    added js in console

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+console.log("JS OK")
+
+// Scrivi un programma che stampi in console i numeri da 1 a 100,
+// per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+
+
+// stampare in console numeri da 1 a 100
+/* per i multipli di 3 stampi “Fizz” al posto del numero e 
+ per i multipli di 5 stampi “Buzz” */
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 !== 0) {
+    console.log("Fizz")
+    }else if (i % 5 === 0 && i % 3 !== 0) {
+    console.log("Buzz")
+    }else if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz")
+    }else{
+        console.log(i)
+    }
+}
 
 
 
