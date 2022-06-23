2d07443b96c87595d4dc576427571c4b2aef69af
Steveh77 <stederosa76@gmail.com>
2022-06-24 00:00:28 +0200

    added bonus (da finire)

--- js/script.js
+++ js/script.js
@@ -9,18 +9,34 @@ console.log("JS OK")
 // stampare in console numeri da 1 a 100
 /* per i multipli di 3 stampi “Fizz” al posto del numero e 
  per i multipli di 5 stampi “Buzz” */
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 !== 0) {
-    console.log("Fizz")
-    }else if (i % 5 === 0 && i % 3 !== 0) {
-    console.log("Buzz")
-    }else if (i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz")
-    }else{
-        console.log(i)
+
+const blackboard = document.getElementById("blackboard")
+const fizz = "Fizz"
+
+function Start() {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 !== 0) {
+            console.log("Fizz")
+            blackboard.innerText += ' ' + 'fizz';
+    
+        }else if (i % 5 === 0 && i % 3 !== 0) {
+            console.log("Buzz")
+            blackboard.innerText += ' ' + 'Buzz';
+    
+        }else if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz")
+            blackboard.innerText += ' ' + 'FizzBuzz';
+    
+        }else{
+            console.log(i) 
+            blackboard.innerText += ' ' + i;
+            
+        }
     }
 }
 
+
+// document.getElementById('blackBoard').innerHTML = '<div class="col-1 cells verde border"></div>'
 
 
 
